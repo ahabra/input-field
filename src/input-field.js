b3faa934c99db261c1f8180af084a6d24c57b1c0
Abdul Habra <abdul.habra@gmail.com>
2021-02-27 16:31:45 -0500

    add some docs

--- src/input-field.js
+++ src/input-field.js
@@ -4,6 +4,29 @@ import {Domer, Objecter, Stringer} from '@techexp/jshelper'
 import {ValidationRules, Rule} from './input-field-validation'
 import template from './input-field.html'
 
+/**
+ * Define a responsive input field with its label.
+ * You can control the field with the following attributes:
+ * required: is the field required. The value should be "required"
+ * required-message: Optional. The message to show for required fields. Default is "Required Field"
+ * minlength: Integer. The minimum number of characters.
+ * minlength-message: Optional. The message to show when there is minlength. Default is "Minimum Length is %v"
+ * maxlength: Integer. Maximum number of characters. The component will prevent entering more.
+ * pattern: RegEx string. The input must match given regex.
+ * pattern-message: Optional. The message to show when there is a pattern requirement. Default is "Must satisfy the pattern %v"
+ * min: Number. Minimum acceptable numeric value.
+ * min-message: Optional. The message to show when there is a min requirement. Default is "Minimum value of %v"
+ * max: Number. Maximum acceptable numeric value.
+ * max-message: Optional. The message to show when there is a max requirement. Default is "Maximum value of %v"
+ *
+ * Additionally, you can add custom validation rules to the component using:
+ *
+ * element.actions.addRule(name, message, validator)
+ * Where validator is a function that takes the current value and returns a boolean.
+ *
+ * @param cssFilePath The path to a CSS file containing the input-field styles. Please use the provided input-field.css
+ * and customize it as needed.
+ */
 export function define(cssFilePath = '') {
 
   webitem.defineElement({
