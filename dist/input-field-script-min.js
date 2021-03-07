0be8cf2fc6e8cc2888ed9f6bfb6c403593023902
Abdul Habra <abdul.habra@gmail.com>
2021-03-07 13:16:30 -0500

    update version

--- dist/input-field-script-min.js
+++ dist/input-field-script-min.js
@@ -1,6 +1,6 @@
 // input-field Web Component. Responsive input field with label and validation
 // https://github.com/ahabra/input-field
-// Copyright 2021 (C) Abdul Habra. Version 0.0.1.
+// Copyright 2021 (C) Abdul Habra. Version 0.1.0.
 // Apache License Version 2.0
 
 
